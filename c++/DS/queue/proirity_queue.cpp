4643fc4d035f81e09d699c85bde7ee7fdfd063aa
sid2199 <siddharth.bhardwaj.22111999@gmail.com>
2021-09-16 09:17:52 +0530

    Queue

--- c++/DS/queue/proirity_queue.cpp
+++ c++/DS/queue/proirity_queue.cpp
@@ -12,8 +12,8 @@ class PriorityQueue {
             PQ = (int*) malloc (sizeof(int)*(n-1));
         }
 
-        void heapifyUp () {
-            int parent, ind = size;
+        void heapifyUp (int from) {
+            int parent, ind = from;
             while (ind > 0) {
                 if (ind%2==0) parent = ((ind-1)/2);
                 else parent = ind/2;
@@ -26,8 +26,8 @@ class PriorityQueue {
             }
         }
 
-        void heapifyDown () {
-            int leftChild, rightChild, ind = 0;
+        void heapifyDown (int from) {
+            int leftChild, rightChild, ind = from;
             while (ind < size) {
                 if (((ind*2)+2) <= size) {
                     rightChild = (ind*2)+2;
@@ -58,17 +58,28 @@ class PriorityQueue {
         void insert (int data) {
             if (size >= (n-1)) PQ = (int*) realloc (PQ, sizeof(int)*((n*=2)-1));
             PQ[++size] = data;
-            heapifyUp();
+            heapifyUp(size);
         }
 
         void extractMax () {
             if (size > -1) {
                 cout << "Max: [" << PQ[0] << "]\n";
                 PQ[0] = PQ[--size];
-                heapifyDown();
+                heapifyDown(0);
                 if (size < (n/2)) PQ = (int*) realloc (PQ, sizeof(int)*(n/=2));
             }
             else cout << "Nothing in the Priority Queue remaining\n";
+        }
+        
+        void remove (int data) {
+            int found = NULL;
+            for (int i=0; i<size; i++) if (PQ[i] == data) found = i;
+            if (found) {
+                cout << "Removed: [" << PQ[found] << "]\n";
+                PQ[found] = PQ[--size];
+                heapifyUp(found);
+                heapifyDown(found);
+            }
         }
 
         void show () {
@@ -81,7 +92,7 @@ class PriorityQueue {
 };
 
 int main () {
-    int prior[] = {0,23,10,1,2,7,4,3,56,12,12,4};
+    int prior[] = {0,23,10,1,2,7,3,65,23,4,4,4,3,56,12,12,4};
     PriorityQueue pq;
     for (auto i: prior) pq.insert(i);
     pq.show();
@@ -89,7 +100,11 @@ int main () {
     pq.show();
     pq.extractMax();
     pq.show();
-    pq.extractMax();
+    pq.remove(12);
+    pq.show();
+    pq.remove(4);
+    pq.show();
+    pq.remove(23);
     pq.show();
     return 0;
 }
